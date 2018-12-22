bd027ec7b6a909eb3c19864a9f0db1db991b0c97
James Montemagno <james.montemagno@gmail.com>
2018-12-22 08:54:56 -0800

    Update to ASP.NET Core 2.2 & .NET Core 2.2

--- Twitch-SignalRSaturdays/XamChat.Backend/Startup.cs
+++ Twitch-SignalRSaturdays/XamChat.Backend/Startup.cs
@@ -26,7 +26,7 @@ namespace XamChat.Backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddCors(options => options.AddPolicy("CorsPolicy", builder =>
             {
               builder.AllowAnyMethod()
